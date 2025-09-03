574129c896032b0aaf22d9e893ded175de5613a9
Gus <garciagus1992@gmail.com>
2025-09-03 16:16:39 +0200

    Update reduccion_dimensionalidad.py

---
+++ reduccion_dimensionalidad.py
@@ -0,0 +1,141 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+import matplotlib as mpl
+
+# creamos unos datos aleatorios
+np.random.seed(4)
+m = 60
+w1, w2 = 0.1, 0.3
+noise = 0.1
+
+angles = np.random.rand(m) * 3 * np.pi / 2 - 0.5
+X = np.empty((m, 3))
+X[:, 0] = np.cos(angles) + np.sin(angles)/2 + noise * np.random.randn(m) / 2
+X[:, 1] = np.sin(angles) * 0.7 + noise * np.random.randn(m) / 2
+X[:, 2] = X[:, 0] * w1 + X[:, 1] * w2 + noise * np.random.randn(m)
+
+
+
+# mostramos el gráfico 3d al tener 3 características
+fig = plt.figure(figsize=(6, 3.8))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(X[:, 0], X[:, 1], X[:, 2], "bo")
+ax.set_xlim([-1.5, 1.3])
+ax.set_ylim([-1.2, 1.2])
+ax.set_zlim([-1, 1])
+plt.show()
+
+
+# centramos los datos
+X_centered = X - X.mean(axis=0)
+U, s, Vt = np.linalg.svd(X_centered)
+print(Vt)
+
+# creamos un dataset bidimiensional al tener solo 2 características
+W2 = Vt.T[:, :2]
+X2D = X_centered.dot(W2)
+
+
+# y mostramos el gráfico bidimensional
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal')
+
+ax.plot(X2D[:, 0], X2D[:, 1], "k+")
+ax.plot(X2D[:, 0], X2D[:, 1], "k.")
+ax.set_xlabel("$z_1$", fontsize=18)
+ax.set_ylabel("$z_2$", fontsize=18, rotation=0)
+ax.axis([-1.5, 1.3, -1.2, 1.2])
+ax.grid(True)
+plt.show()
+
+
+# usamos sklearn para redimensionar al número que queramos
+pca = PCA(n_components = 2)
+X2D = pca.fit_transform(X)
+
+# creamos el paso inverso pasar de 2 a 3 dimensiones
+X3D_inv = pca.inverse_transform(X2D)
+
+fig = plt.figure(figsize=(6, 3.8))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(X3D_inv[:, 0], X3D_inv[:, 1], X3D_inv[:, 2], "bo")
+ax.set_xlim([-1.5, 1.3])
+ax.set_ylim([-1.2, 1.2])
+ax.set_zlim([-1, 1])
+plt.show()
+
+
+
+# como poder elegir el número correcto de dimensiones con un valor mínimo de pérdida
+cumsum = np.cumsum(pca.explained_variance_ratio_)
+d = np.argmax(cumsum >= 0.95) + 1
+print(d)
+
+
+# usamos el dataset mnist 
+mnist = fetch_openml('mnist_784', version=1)
+mnist.target = mnist.target.astype(np.uint8)
+
+X = mnist["data"].values
+y = mnist["target"].values
+
+# los separamos entre train y test
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# usamos el PCA para ver a cuantas dimensiones lo podemos reducir
+pca = PCA()
+pca.fit(X_train)
+cumsum = np.cumsum(pca.explained_variance_ratio_)
+d = np.argmax(cumsum >= 0.95) + 1
+print(d)
+
+
+# y mostramos el gráfico
+plt.figure(figsize=(6,4))
+plt.plot(cumsum, linewidth=3)
+plt.axis([0, 784, 0, 1])
+plt.xlabel("Dimensions")
+plt.ylabel("Explained Variance")
+plt.plot([d, d], [0, 0.95], "k:")
+plt.plot([0, d], [0.95, 0.95], "k:")
+plt.plot(d, 0.95, "ko")
+plt.grid(True)
+plt.show()
+
+
+# usamos pca para reducir las dimensiones
+pca = PCA(n_components=0.95)
+X_reduced = pca.fit_transform(X_train)
+
+
+# y podemos recuperar las dimensiones originales
+X_recovered = pca.inverse_transform(X_reduced)
+
+# mostramos el gráfico original y el redimensionado
+def plot_digits(instances, images_per_row=5, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = mpl.cm.binary, **options)
+    plt.axis("off")
+
+plt.figure(figsize=(7, 4))
+plt.subplot(121)
+plot_digits(X_train[::2100])
+plt.title("Original", fontsize=16)
+plt.subplot(122)
+plot_digits(X_recovered[::2100])
+plt.title("Comprimido", fontsize=16)
+plt.tight_layout()
