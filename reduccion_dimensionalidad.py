bb443423af027035bfe7b2829fece767a6f36c29
Gus <garciagus1992@gmail.com>
2025-09-04 16:48:56 +0200

    add requeriments.txt and otras técnicas de reducción

--- reduccion_dimensionalidad.py
+++ reduccion_dimensionalidad.py
@@ -5,6 +5,11 @@ from sklearn.decomposition import PCA
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 import matplotlib as mpl
+from sklearn.datasets import make_swiss_roll
+from sklearn.manifold import LocallyLinearEmbedding
+from sklearn.manifold import MDS
+from sklearn.manifold import Isomap
+from sklearn.manifold import TSNE
 
 # creamos unos datos aleatorios
 np.random.seed(4)
@@ -139,4 +144,70 @@ plt.subplot(122)
 plot_digits(X_recovered[::2100])
 plt.title("Comprimido", fontsize=16)
 plt.tight_layout()
+plt.show()
+
+
+# ---- Otras técnicas ----
+
+# creamos unos datos con el make_swiss_roll
+X, t = make_swiss_roll(n_samples=1000, noise=0.2, random_state=41)
+
+# y mostramos el gráfico
+axes = [-11.5, 14, -2, 23, -12, 15]
+
+fig = plt.figure(figsize=(6, 5))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=t, cmap=plt.cm.hot)
+ax.view_init(10, -70)
+ax.set_xlabel("$x_1$", fontsize=18)
+ax.set_ylabel("$x_2$", fontsize=18)
+ax.set_zlabel("$x_3$", fontsize=18)
+ax.set_xlim(axes[0:2])
+ax.set_ylim(axes[2:4])
+ax.set_zlim(axes[4:6])
+
+plt.show()
+
+# instanciamos el locallyLinearEmdedding con sus parámetros
+lle = LocallyLinearEmbedding(n_components=2, n_neighbors=10, random_state=42)
+X_reduced = lle.fit_transform(X)
+
+
+# y mostramos el gráfico
+plt.title("Unrolled swiss roll using LLE", fontsize=14)
+plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=t, cmap=plt.cm.hot)
+plt.xlabel("$z_1$", fontsize=18)
+plt.ylabel("$z_2$", fontsize=18)
+plt.axis([-0.065, 0.055, -0.1, 0.12])
+plt.grid(True)
+plt.show()
+
+
+# instanciamos varios modelos de reducción
+mds = MDS(n_components=2, random_state=42)
+X_reduced_mds = mds.fit_transform(X)
+
+isomap = Isomap(n_components=2)
+X_reduced_isomap = isomap.fit_transform(X)
+
+tsne = TSNE(n_components=2, random_state=42)
+X_reduced_tsne = tsne.fit_transform(X)
+
+
+# y los mostramos en un gráfico
+titles = ["MDS", "Isomap", "t-SNE"]
+
+plt.figure(figsize=(11,4))
+
+for subplot, title, X_reduced in zip((131, 132, 133), titles,
+                                     (X_reduced_mds, X_reduced_isomap, X_reduced_tsne)):
+    plt.subplot(subplot)
+    plt.title(title, fontsize=14)
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=t, cmap=plt.cm.hot)
+    plt.xlabel("$z_1$", fontsize=18)
+    if subplot == 131:
+        plt.ylabel("$z_2$", fontsize=18, rotation=0)
+    plt.grid(True)
+
 plt.show()
